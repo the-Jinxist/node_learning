e6c0e06f5f6aaa33190586f3b0e04daf4e4fc4f1
the-Jinxist <nerosilva522@gmail.com>
2021-06-17 13:16:26 +0100

    Finished lecture on node core

--- node_core/node_core_learning.js
+++ node_core/node_core_learning.js
@@ -3,8 +3,18 @@
 
 console.log(); // global
 
-setTimeout(); // Calling a function after a delay
-clearTimeout();
+// setTimeout(); // Calling a function after a delay
+// clearTimeout();
 
-setInterval(); //Repetitively call a function after a delay
-clearInterval();
+// setInterval(); //Repetitively call a function after a delay
+// clearInterval();
+
+var message = '';
+console.log(globalThis.message); //The variable is not added to the global object. Returns undefined
+
+
+
+
+
+
+
