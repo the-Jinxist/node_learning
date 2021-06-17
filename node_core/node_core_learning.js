c893a0b564dc9ad46cadf052122c2044e36fe896
the-Jinxist <nerosilva522@gmail.com>
2021-06-17 13:09:18 +0100

    Working on global objects

---
+++ node_core/node_core_learning.js
@@ -0,0 +1,10 @@
+
+//Learning global objects
+
+console.log(); // global
+
+setTimeout(); // Calling a function after a delay
+clearTimeout();
+
+setInterval(); //Repetitively call a function after a delay
+clearInterval();
