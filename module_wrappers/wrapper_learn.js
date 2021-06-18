4073b25a0869994d0aab93dd5ad07675825820ee
the-Jinxist <nerosilva522@gmail.com>
2021-06-18 10:56:20 +0100

    Looked at the module wrapper function

---
+++ module_wrappers/wrapper_learn.js
@@ -0,0 +1,8 @@
+
+
+//At run-time our code is called like this
+//This function is called the module wrapper function
+
+(function (exports, require, modue, __filename, __dirnae){
+    console.log("Hey brother");
+})
