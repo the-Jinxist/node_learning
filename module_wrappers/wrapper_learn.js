2e912e7cfe0f76866c0fab78bf6deaa11f5ae213
the-Jinxist <nerosilva522@gmail.com>
2021-06-18 10:59:10 +0100

    Looked at the values of the first-class methods in the module wrapper function

--- module_wrappers/wrapper_learn.js
+++ module_wrappers/wrapper_learn.js
@@ -1,8 +1,4 @@
 
+console.log(__filename);
 
-//At run-time our code is called like this
-//This function is called the module wrapper function
-
-(function (exports, require, modue, __filename, __dirnae){
-    console.log("Hey brother");
-})
+console.log(__dirname);
