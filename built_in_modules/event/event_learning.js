c933b83c733550564c247f9652dc02873f4e72ee
the-Jinxist <nerosilva522@gmail.com>
2021-06-18 21:36:33 +0100

    Added event arguments

--- built_in_modules/event/event_learning.js
+++ built_in_modules/event/event_learning.js
@@ -2,10 +2,12 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 //Registering a listener
-emitter.on('messageLogged', function(){
-    console.log('Gotten message!');
+emitter.on('messageLogged', function(arg){
+    console.log('Gotten message!', arg);
 });
 
 
 //Signalling that an event has happened
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {
+    id: 1, url: "monkey"
+});
