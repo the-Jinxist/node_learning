c29c6098f4377d1b16e80e124c4f0221cfee14df
the-Jinxist <nerosilva522@gmail.com>
2021-06-21 13:26:39 +0100

    Learnt about PORTs

--- exp_learn/exp_learn.js
+++ exp_learn/exp_learn.js
@@ -8,11 +8,16 @@ app.get(
 (request, response) => {
     
     //Sending back a response
-    response.send('Hello World');
+    response.send('Hello World!!!');
 });
 
 app.get('/api/courses',(request, response) => {
     response.send([1, 2, 3, 4, 5, 6]);
 });
 
+// 3000 is an arbitrary number and since hosting site assign their own ports,
+//.. we can rely on 3000 to be available. So we have to use an environment variable
+// Usually in hosting applications we have this environment variable called PORT
+
+//We use the global process object for reading this PORT variable
 app.listen(3000, () => console.log('Listening on port 3000'));
