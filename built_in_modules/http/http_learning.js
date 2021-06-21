7708c112c413b28270c1e68ae1bb5f061fcc9011
the-Jinxist <nerosilva522@gmail.com>
2021-06-21 12:35:20 +0100

    Moving to learning express

--- built_in_modules/http/http_learning.js
+++ built_in_modules/http/http_learning.js
@@ -2,10 +2,19 @@ const http = require('http');
 
 //Here we're creating a web server
 //This server is also an event emitter
-const server = http.createServer();
 
-server.on('connection', (socket) => {
-    console.log('New connection', socket)
+//Now we're using callbacks, instead of the on method
+const server = http.createServer((req, res) => {
+    if(req.url == '/'){
+        res.write('Hello World');
+        res.end();
+    }
+
+    //Incase of a particular path or endpoint
+    if(req.url == '/api/courses'){
+        res.write(JSON.stringify([1,2,3,4,5,6]));
+        res.end();
+    }
 });
 
 server.listen(3000);
