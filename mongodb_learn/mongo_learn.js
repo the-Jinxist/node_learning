dc9c2072ffa8a27c9f0b8104672e1b9e58cb6d85
the-Jinxist <nerosilva522@gmail.com>
2021-06-22 14:00:03 +0100

    First ever connection to mondodb

--- mongodb_learn/mongo_learn.js
+++ mongodb_learn/mongo_learn.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
+
+//Omo, you must encode the special characters in your connection string 
+//..before you use it o. Encoded '#' as '%23'
+const connectionString = 
+    'mongodb+srv://neo_dev:<goodness11%23>@neo-mongo.fhr3f.mongodb.net/neodb?retryWrites=true&w=majority'
 
 //Allows for easy parsing of request bodies.
 app.use(express.json());
@@ -9,6 +16,16 @@ app.use('/posts', () => {
     console.log('This is a middle ware running');
 });
 
+MongoClient.connect(connectionString, (error, client) => {
+    if(error){
+        console.log(`Error occurred while connecting to mongo db, ${error}`);
+        return;
+    }
+
+    const db = client.db('neodb');
+    const neosCollection = db.collection('neoscollection')
+
+});
 
 
 //We have to start listening to a server
