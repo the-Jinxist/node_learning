c217128ace0e9912fb7ea6570ee79376f8a8b541
the-Jinxist <nerosilva522@gmail.com>
2021-06-23 13:23:21 +0100

    Added gitignore file

--- mongodb_learn/app_routes/posts.js
+++ mongodb_learn/app_routes/posts.js
@@ -3,8 +3,14 @@ const express = require('express');
 //Using a router to cleanly separate app logic into separate files
 const router = express.Router();
 
-router.get('/posts', (request, response) => {
-    response.send('We are still on posts, sadly');
+//We can replace '/posts' with '/' because we have specified the route that will
+//..will be used in @mongo_learn.js
+router.get('/', (request, response) => {
+    response.send('We are still on posts in a new route, sadly');
+});
+
+router.get('/specific', (request, response) => {
+    response.send('Here is your specific post, ooh rah!');
 });
 
 module.exports = router;
