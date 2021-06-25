776d4897a9178336d06fd603df183add1229d672
the-Jinxist <nerosilva522@gmail.com>
2021-06-25 14:59:38 +0100

    Adding user to database done. Pending validation

--- mongodb_learn/app_routes/auth.js
+++ mongodb_learn/app_routes/auth.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
 router.post('/register', async (request, response) => {
-    response.send('Omo, so quickly!');
+   
+    const user = new User({
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password
+    })
+
+    try{
+        const savedUser = await user.save();
+        response.send(savedUser.toString());
+    }catch(e){
+        console.log(e.toString());
+        response.status(400).send(e);
+    }
 })
 
 module.exports = router;
