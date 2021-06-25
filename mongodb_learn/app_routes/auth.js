bc271ef019939f3ca161d653e39995e54c72841f
the-Jinxist <nerosilva522@gmail.com>
2021-06-25 12:41:12 +0100

    Routes work fine

--- mongodb_learn/app_routes/auth.js
+++ mongodb_learn/app_routes/auth.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/register', async (request, response) => {
+    response.send('Omo, so quickly!');
+})
 
 module.exports = router;
