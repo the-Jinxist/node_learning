07639abefc85334d9292b73c0d8616b831a99c2c
the-Jinxist <nerosilva522@gmail.com>
2021-07-02 11:14:34 +0100

    Implemented login functionality

--- mongodb_learn/app_routes/auth.js
+++ mongodb_learn/app_routes/auth.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const router = express.Router();
 
 const User = require('../models/User');
-const Joi = require('joi');
-const { SchemaType } = require('mongoose');
 const { validateSignUpRequest, validateLoginRequest } = require('../models/validation');
+const bcrypt = require('bcryptjs');
 
+const router = express.Router();
 
 router.post('/register', async (request, response) => {
    
@@ -22,7 +21,7 @@ router.post('/register', async (request, response) => {
     }
 
     //Checking if the user is already in the database
-    const emailExists = User.findOne({email: request.body.email});
+    const emailExists = await User.findOne({email: request.body.email});
     if(emailExists){
         response.status(400).json({
             status: 400,
@@ -32,18 +31,29 @@ router.post('/register', async (request, response) => {
         return;
     }
 
+    //Hash the password
+    //10 describes the complexity of the encoder of the password
+    const salt = await bcrypt.genSalt(10);
+
+    //The salt combines with the password to create a mumble of string that
+    //..only bcrypt can decrypt. Ooh rah!
+    const hashedPassword = await bcrypt.hash(request.body.password, salt);
+
     //Creating a new user
     const user = new User({
         name: request.body.name,
         email: request.body.email,
-        password: request.body.password
+        password: hashedPassword
     })
 
         const savedUser = await user.save();
         response.status(200).json({
             status: 200,
             message: "Sign Up Successful!",
-            data: savedUser
+            data: {
+                id: savedUser.id,
+                email: savedUser.email
+            }
         });
     }catch(e){
         console.log(e.toString());
@@ -52,6 +62,59 @@ router.post('/register', async (request, response) => {
             message: e.toString()
         });
     }
+});
+
+router.post('/login', async (request, response) => {
+
+    try{
+        const validation = validateLoginRequest(request.body);
+        if(validation.error){
+            console.log(validation.error.details);
+            response.status(400).json({
+                message: validation.error.details[0].message
+            });
+            return; 
+        }
+        
+        const savedUser = await User.findOne({email: request.body.email})
+        if(savedUser){
+            const validPass = await bcrypt.compare(request.body.password, savedUser.password);
+            if(validPass){
+                response.status(200).json({
+                    status: 200,
+                    message: "Log in Successful!",
+                    data: {
+                        id: savedUser.id,
+                        email: savedUser.email
+                    }
+                });
+
+                return;
+            }else{
+                
+                response.status(400).json({
+                    message: 'Email or Password is incorrect'
+                });
+
+                return;
+            }
+            
+        }else{
+            response.status(400).json({
+                message: 'Email or Password is incorrect'
+            });
+            return;
+        }
+
+    }catch(e){
+        console.log(e.toString());
+        response.status(400).json({
+            status: 400,
+            message: e.toString()
+        });
+    }
+    
+
 });
 
 module.exports = router;
