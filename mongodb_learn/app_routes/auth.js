767870fc5de6b27cc999c66bfeaac2b1ad10eb22
Favour Olukayode <nerosilva522@gmail.com>
2021-07-13 14:08:15 +0100

    Added token generation to sign up duh!

--- mongodb_learn/app_routes/auth.js
+++ mongodb_learn/app_routes/auth.js
@@ -12,51 +12,55 @@ router.post('/register', async (request, response) => {
    
     try{
 
-    //Validating the data collected from the user
-    const validation = validateSignUpRequest(request.body);
-    if(validation.error){
-        console.log(validation.error.details);
-        response.status(400).json({
-            message: validation.error.details[0].message
-        });
-        return;
-    }
+        //Validating the data collected from the user
+        const validation = validateSignUpRequest(request.body);
+        if(validation.error){
+            console.log(validation.error.details);
+            response.status(400).json({
+                message: validation.error.details[0].message
+            });
+            return;
+        }
 
-    //Checking if the user is already in the database
-    const emailExists = await User.findOne({email: request.body.email});
-    if(emailExists){
-        response.status(400).json({
-            status: 400,
-            message: `A user with the email ${request.body.email} already exists`
-        }); 
+        //Checking if the user is already in the database
+        const emailExists = await User.findOne({email: request.body.email});
+        if(emailExists){
+            response.status(400).json({
+                status: 400,
+                message: `A user with the email ${request.body.email} already exists`
+            }); 
 
-        return;
-    }
+            return;
+        }
 
-    //Hash the password
-    //10 describes the complexity of the encoder of the password
-    const salt = await bcrypt.genSalt(10);
+        //Hash the password
+        //10 describes the complexity of the encoder of the password
+        const salt = await bcrypt.genSalt(10);
 
-    //The salt combines with the password to create a mumble of string that
-    //..only bcrypt can decrypt. Ooh rah!
-    const hashedPassword = await bcrypt.hash(request.body.password, salt);
+        //The salt combines with the password to create a mumble of string that
+        //..only bcrypt can decrypt. Ooh rah!
+        const hashedPassword = await bcrypt.hash(request.body.password, salt);
 
-    //Creating a new user
-    const user = new User({
-        name: request.body.name,
-        email: request.body.email,
-        password: hashedPassword
-    })
+        //Creating a new user
+        const user = new User({
+            name: request.body.name,
+            email: request.body.email,
+            password: hashedPassword
+        })
 
         const savedUser = await user.save();
+        const token = jwt.sign({_id: savedUser._id}, process.env.TOKEN_SECRET);
+        
         response.status(200).json({
             status: 200,
             message: "Sign Up Successful!",
             data: {
                 id: savedUser.id,
-                email: savedUser.email
+                email: savedUser.email,
+                token: token
             }
         });
+
     }catch(e){
         console.log(e.toString());
         response.status(400).json({
@@ -84,7 +88,6 @@ router.post('/login', async (request, response) => {
             if(validPass){
 
                 //Create and assign a token
-                console.log(`Fancy secret toke: ${process.env.TOKEN_SECRET}`);
                 const token = jwt.sign({_id: savedUser._id}, process.env.TOKEN_SECRET);
 
                 response.header('auth-token', token).status(200).json({
--- mongodb_learn/app_routes/verify_token.js
+++ mongodb_learn/app_routes/verify_token.js
@@ -1,5 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 function auth (request, response, next){
-    //1:00:54
+    const token = request.header('auth-token');
+    if(!token) return response.status(401).send('Access Denied');
+
+    try{
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        request.user = verified;
+    }catch(err){
+        request.status(400).send(`Invalid Token: ${err}`);
+    }
+
 }
